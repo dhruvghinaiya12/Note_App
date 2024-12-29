5458dd441dc500dc45e1a16cc155cb0cc1564255
dhruvghinaiya12 <dhruvghinaiya12@gmail.com>
2024-12-29 14:27:21 +0530

    Note-App

--- Notes_App/config/db.js
+++ Notes_App/config/db.js
@@ -1,20 +1,17 @@
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", false); // Make sure strictQuery is set before using mongoose
+mongoose.set("strictQuery", false); 
 require("dotenv").config();
 
 const url = process.env.DB_URL;
 
 const db = async () => {
   try {
-    // Including some options for connection
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    
+    await mongoose.connect(url);
     console.log("Connected to database");
   } catch (error) {
     console.error("Error connecting to database:", error);
-    process.exit(1); // Exit the process if connection fails
+    process.exit(1); 
   }
 };
 
